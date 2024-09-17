f5e16c4047a0584e16d73d5ed31eb379ef8f6975
Matheus Pedroni <pnmatheus@protonmail.com>
2024-09-16 21:42:49 -0300

    include label in split

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def _class_probabilities(labels):
 def _find_best_split(X, y):
 
     min_split_entropy = 1e9
-    best_split = best_feat = 0
+    best_split = best_feat = left_label = right_label = 0
     best_left = best_right = None
 
     for feat_idx in range(X.shape[1]):
@@ -68,22 +68,36 @@ def _find_best_split(X, y):
                 best_feat = feat_idx
                 best_left = left
                 best_right = right
+                left_label = (np.mean(y_sort[:idx]) >= 0.5).astype(int)
+                right_label = (left_label + 1) % 2
 
-    return best_feat, best_split, best_left, best_right
+    return (
+        min_split_entropy,
+        best_feat,
+        best_split,
+        best_left,
+        best_right,
+        left_label,
+        right_label,
+    )
 
 
-def split_node(node, X, y):
+def split_node(node, X, y, value):
     if X.shape[0] == 1:
-        return LeafNode(0)  # XXX value
+        return LeafNode(value)
 
-    feat_idx, best_split, left, right = _find_best_split(X, y)
+    min_entropy, feat_idx, best_split, left, right, left_label, right_label = (
+        _find_best_split(X, y)
+    )
+
     X_left = X[left, :]
     X_right = X[right, :]
     y_left = y[left]
     y_right = y[right]
-    node = Node(feat_idx, best_split, LeafNode(0), LeafNode(1))
-    node.left = split_node(node, X_left, y_left)
-    node.right = split_node(node, X_right, y_right)
+    node = Node(feat_idx, best_split, LeafNode(left_label), LeafNode(right_label))
+
+    node.left = split_node(node, X_left, y_left, left_label)
+    node.right = split_node(node, X_right, y_right, right_label)
     return node
 
 

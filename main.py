862e057c815687b81cd2cbf39cf454b7f7e891d5
Matheus Pedroni <pnmatheus@protonmail.com>
2024-09-19 21:48:27 -0300

    change class label to probabilities

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 
 @dataclass
 class LeafNode:
-    value: int
+    prob: float
 
 
 @dataclass
@@ -34,7 +34,7 @@ def _class_probabilities(labels):
 def _find_best_split(X, y):
 
     min_split_entropy = 1e9
-    best_split = best_feat = left_label = right_label = 0
+    best_split = best_feat = left_prob = right_prob = 0
     best_left = best_right = None
 
     for feat_idx in range(X.shape[1]):
@@ -57,8 +57,8 @@ def _find_best_split(X, y):
                 best_feat = feat_idx
                 best_left = left
                 best_right = right
-                left_label = (np.mean(y_sort[:idx]) >= 0.5).astype(int)
-                right_label = (np.mean(y_sort[idx:]) >= 0.5).astype(int)
+                left_prob = np.mean(y_sort[:idx])
+                right_prob = np.mean(y_sort[idx:])
 
     return (
         min_split_entropy,
@@ -66,8 +66,8 @@ def _find_best_split(X, y):
         best_split,
         best_left,
         best_right,
-        left_label,
-        right_label,
+        left_prob,
+        right_prob,
     )
 
 
@@ -79,7 +79,7 @@ def split_node(node, X, y, value, depth, max_depth):
     if prior_entropy == 0:
         return LeafNode(value)
 
-    split_entropy, feat_idx, best_split, left, right, left_label, right_label = (
+    split_entropy, feat_idx, best_split, left, right, left_prob, right_prob = (
         _find_best_split(X, y)
     )
     # print(f"information gain: {prior_entropy - split_entropy:.2f}")
@@ -88,17 +88,17 @@ def split_node(node, X, y, value, depth, max_depth):
     X_right = X[right, :]
     y_left = y[left]
     y_right = y[right]
-    node = Node(feat_idx, best_split, LeafNode(left_label), LeafNode(right_label))
+    node = Node(feat_idx, best_split, LeafNode(left_prob), LeafNode(right_prob))
 
-    node.left = split_node(node, X_left, y_left, left_label, depth + 1, max_depth)
-    node.right = split_node(node, X_right, y_right, right_label, depth + 1, max_depth)
+    node.left = split_node(node, X_left, y_left, left_prob, depth + 1, max_depth)
+    node.right = split_node(node, X_right, y_right, right_prob, depth + 1, max_depth)
     return node
 
 
 def print_tree(node, depth=0):
     indent = "  " * depth
     if isinstance(node, LeafNode):
-        print(f"{indent}LeafNode(value={node.value})")
+        print(f"{indent}LeafNode(value={node.prob})")
     else:
         print(
             f"{indent}Node(feature_idx={node.feature_idx}, split_value={node.split_value:.2f})"
@@ -120,9 +120,15 @@ def predict(root: Node | LeafNode, X: np.ndarray) -> np.ndarray:
             else:
                 node = node.right
 
-        y_pred.append(node.value)
+        y_pred.append(node.prob)
 
     return np.array(y_pred)
+
+
+def predict_class(root: Node | LeafNode, X: np.ndarray) -> np.ndarray:
+    pred_prob = predict(root, X)
+    pred = (pred_prob >= 0.5).astype(int)
+    return pred
 
 
 if __name__ == "__main__":
@@ -134,7 +140,7 @@ if __name__ == "__main__":
     y = np.repeat([0, 1], 100)
     node = LeafNode(0)
     node = split_node(node, X, y, 0, depth=0, max_depth=5)
-    pred = predict(node, X)
+    pred = predict_class(node, X)
     print(node)
     print_tree(node)
     print(pred)

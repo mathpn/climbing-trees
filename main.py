aeb71a361524dd7fb6e423b41bddad4aedc2a018
Matheus Pedroni <pnmatheus@protonmail.com>
2024-10-23 22:08:45 -0300

    remove dead code

--- main.py
+++ main.py
@@ -321,7 +321,7 @@ class RandomForestClassifier:
         self.sample_proportion = sample_proportion
         self.max_depth = max_depth
         self.min_info_gain = min_info_gain
-        self._trees: list[Node | LeafNode] = []
+        self._trees: list[DecisionTreeClassifier] = []
         self._col_idx: list[np.ndarray] = []
 
     def fit(self, X, y) -> None:
@@ -335,9 +335,8 @@ class RandomForestClassifier:
                 np.arange(X.shape[1]), size=n_features, replace=False
             )
             X_sample = X[:, idx]
-            tree = train_classification_tree(
-                *_sample(X_sample, y, n), self.max_depth, self.min_info_gain
-            )
+            tree = DecisionTreeClassifier(self.max_depth, self.min_info_gain)
+            tree.fit(*_sample(X_sample, y, n))
             self._trees.append(tree)
             self._col_idx.append(idx)
 
@@ -349,7 +348,8 @@ class RandomForestClassifier:
             raise ValueError("model must be trained before prediction")
 
         preds = [
-            predict(tree, X[:, idx]) for tree, idx in zip(self._trees, self._col_idx)
+            tree.predict_proba(X[:, idx])
+            for tree, idx in zip(self._trees, self._col_idx)
         ]
         pred_arr = np.stack(preds, axis=2)
         return pred_arr.mean(axis=2)
@@ -443,45 +443,6 @@ def _uniform_sample_weights(X):
     return np.ones((X.shape[0], 1)) / X.shape[0]
 
 
-def train_classification_tree(
-    X, y, max_depth: int, min_info_gain: float, sample_weights=None
-) -> Node | LeafNode:
-    y_oh = _one_hot_encode(y)
-    node = LeafNode(np.zeros(y_oh.shape[1]))
-    trained_node = split_node(
-        node,
-        X,
-        y_oh,
-        np.mean(y, axis=0),
-        depth=0,
-        max_depth=max_depth,
-        criterion_fn=_entropy_criterion,
-        sample_weights=sample_weights,
-        min_criterion_reduction=min_info_gain,
-    )
-    node = trained_node if trained_node is not None else node
-    return node
-
-
-def train_regression_tree(
-    X, y, max_depth: int, min_info_gain: float, sample_weights=None
-) -> Node | LeafNode:
-    node = LeafNode(np.zeros(y.shape[1]))
-    trained_node = split_node(
-        node,
-        X,
-        y,
-        np.mean(y, axis=0),
-        depth=0,
-        max_depth=max_depth,
-        criterion_fn=_mse_criterion,
-        min_criterion_reduction=min_info_gain,
-        sample_weights=sample_weights,
-    )
-    node = trained_node if trained_node is not None else node
-    return node
-
-
 def _sample(X, y, sample_size: int) -> tuple[np.ndarray, np.ndarray]:
     idx = np.random.choice(range(X.shape[0]), size=sample_size, replace=True)
 
@@ -489,23 +450,6 @@ def _sample(X, y, sample_size: int) -> tuple[np.ndarray, np.ndarray]:
     y_sample = y[idx]
 
     return X_sample, y_sample
-
-
-def train_random_forest(
-    X, y, n_trees: int, sample_proportion: float, max_depth: int, min_info_gain: float
-):
-    n = math.floor(X.shape[0] * sample_proportion)
-    n_features = math.floor(math.sqrt(X.shape[1]))
-    trees = []
-    for _ in range(n_trees):
-        idx = np.random.choice(np.arange(X.shape[1]), size=n_features, replace=False)
-        X_sample = X[:, idx]
-        tree = train_classification_tree(
-            *_sample(X_sample, y, n), max_depth, min_info_gain
-        )
-        trees.append((tree, idx))
-
-    return trees
 
 
 def _reweight_samples_adaboost(y, pred, sample_weights):
@@ -518,46 +462,12 @@ def _reweight_samples_adaboost(y, pred, sample_weights):
     return sample_weights
 
 
-def train_adaboost(X, y, iterations: int):
-    learners = []
-    sample_weights = np.ones((X.shape[0], 1)) / X.shape[0]
-    for _ in range(iterations):
-        # TODO parameters
-        learner = train_classification_tree(X, y, 2, 0, sample_weights=sample_weights)
-        learners.append(learner)
-        pred = _prob_to_class(predict(learner, X))
-        sample_weights = _reweight_samples_adaboost(y, pred, sample_weights)
-
-    return learners
-
-
 def softmax(x):
     return np.exp(x) / np.exp(x).sum(axis=1, keepdims=True)
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-
-
-def train_gradient_boosting(X, y, iterations: int, learning_rate: float):
-    learners = []
-
-    y_oh = _one_hot_encode(y)
-    activation_fn = sigmoid if y_oh.shape[1] == 1 else softmax
-    raw_pred = np.zeros_like(y_oh, dtype=np.float32)
-    prob = activation_fn(raw_pred)
-
-    for _ in range(iterations):
-        # TODO parameters
-        pseudo_residual = y_oh - prob
-        learner = train_regression_tree(X, pseudo_residual, 1, 0)
-        learner_pred = predict(learner, X)
-        # TODO update predictions
-        raw_pred += learning_rate * learner_pred
-        prob = activation_fn(raw_pred)
-        learners.append(learner)
-
-    return (raw_pred, learning_rate, learners)
 
 
 def print_tree(node: Node | LeafNode, depth: int = 0):
@@ -572,51 +482,6 @@ def print_tree(node: Node | LeafNode, depth: int = 0):
         print_tree(node.left, depth + 1)
         print(f"{indent}Right:")
         print_tree(node.right, depth + 1)
-
-
-def predict(root: Node | LeafNode, X: np.ndarray) -> np.ndarray:
-    y_pred = []
-    for x in X:
-        node = root
-
-        while isinstance(node, Node):
-            if x[node.feature_idx] < node.split_value:
-                node = node.left
-            else:
-                node = node.right
-
-        y_pred.append(node.value)
-
-    return np.array(y_pred)
-
-
-def predict_ensemble(trees: list[Node | LeafNode], X: np.ndarray) -> np.ndarray:
-    preds = [predict(root, X) for root in trees]
-    pred_arr = np.stack(preds, axis=2)
-    return pred_arr.mean(axis=2)
-
-
-def predict_random_forest(
-    model: list[tuple[Node | LeafNode, np.ndarray]], X: np.ndarray
-) -> np.ndarray:
-    preds = [predict(root, X[:, idx]) for root, idx in model]
-    pred_arr = np.stack(preds, axis=2)
-    return pred_arr.mean(axis=2)
-
-
-def predict_gradient_boosting(
-    model: tuple[np.ndarray, float, list[Node | LeafNode]], X: np.ndarray
-) -> np.ndarray:
-    base_value, learning_rate, learners = model
-    pred = base_value + learning_rate * np.sum(
-        [predict(learner, X) for learner in learners], axis=0
-    )
-    if pred.shape[1] == 1:
-        pred = sigmoid(pred)
-    else:
-        pred = softmax(pred)
-
-    return pred
 
 
 def _prob_to_class(prob: np.ndarray) -> np.ndarray:

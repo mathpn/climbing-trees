102e5d0677abebeb026ebda435f61b6cd8476a76
Matheus Pedroni <pnmatheus@protonmail.com>
2025-01-28 20:01:41 -0300

    tidy main script

--- main.py
+++ main.py
@@ -1,123 +1,44 @@
 from __future__ import annotations
 
-import numpy as np
-from sklearn.datasets import (
-    fetch_california_housing,
-    load_breast_cancer,
-    load_diabetes,
-    load_wine,
-)
-from sklearn.metrics import accuracy_score, f1_score, mean_squared_error, roc_auc_score
+import random
 
-from src.cart import (
-    DecisionTreeRegressor,
-    DecisionTreeClassifier,
-)
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer, load_diabetes
+from sklearn.metrics import accuracy_score, f1_score, mean_squared_error
+
+from src.cart import DecisionTreeClassifier, DecisionTreeRegressor, print_tree
 
 
 def main():
-    import random
-
-    from sklearn.tree import DecisionTreeClassifier as DTC
-    from sklearn.tree import DecisionTreeRegressor as DTR
-    from sklearn.tree import plot_tree
-
     random.seed(42)
     np.random.seed(42)
     # X, y = load_wine(return_X_y=True)
     X, y = load_breast_cancer(return_X_y=True)
-    # X = np.random.random(size=(20, 4))
-    # y = np.array([0] * 10 + [1] * 10)
-    # print(X)
-    # print(y)
-    max_depth = 6
-    min_samples_leaf = 1
+    X = pd.DataFrame(np.random.random(size=(20, 4)))
+    X["cat"] = np.array([["B"] * 9 + ["A"] * 6 + ["C"] * 5]).T
+    y = np.array([0] * 10 + [1] * 10)
+
+    max_depth = 4
+    min_samples_leaf = 3
 
     tree = DecisionTreeClassifier(max_depth, min_samples_leaf)
-    tree.fit(X, y)
-    pred_proba = tree.predict_proba(X)
+    tree.fit(X, y, sample_weights=np.ones(len(y)) / 10)
     pred = tree.predict(X)
-    print(y)
-    print(pred)
     score = f1_score(y, pred, average="macro")
     acc = accuracy_score(y, pred)
-    node_count = tree.node_count()
-    tree.print_tree()
-    print(f"tree -> F1: {score:.2f} accuracy: {acc:.2%} with {node_count} nodes")
-
-    tree = DTC(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
-    tree.fit(X, y)
-    pred_proba = tree.predict_proba(X)
-    pred = tree.predict(X)
-    print(y)
-    print(pred)
-    score = f1_score(y, pred, average="macro")
-    acc = accuracy_score(y, pred)
-    print(f"tree -> F1: {score:.2f} accuracy: {acc:.2%}")
-    plot_tree(tree)
-    import matplotlib.pyplot as plt
-
-    plt.savefig("tree.svg")
+    print_tree(tree._root_node)
+    print(f"classification tree -> F1: {score:.2f} accuracy: {acc:.2%}")
 
     X, y = load_diabetes(return_X_y=True)
-    # X = np.random.random((len(y), 4))
-    # X = np.array([[1, 1, 2, 2], [1, 2, 3, 4]]).T
-    # y = np.array([0, 0, 1, 1])
-    # print(X)
-    # print(y)
+    X = pd.DataFrame(X)
 
     tree = DecisionTreeRegressor(max_depth, min_samples_leaf)
     tree.fit(X, y)
     pred = tree.predict(X)
     mse = mean_squared_error(y, pred)
-    print(f"tree -> MSE: {mse:.2f} baseline = {np.mean((y - np.mean(y)) ** 2):.2f}")
-    # print_tree(tree._root_node)
-
-    tree = DTR(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
-    tree.fit(X, y)
-    pred = tree.predict(X)
-    mse = mean_squared_error(y, pred)
-    print(f"tree -> MSE: {mse:.2f} baseline = {np.mean((y - np.mean(y)) ** 2):.2f}")
-    # print_tree(tree._root_node)
-    return
-
-    bagging = BaggingClassifier(
-        lambda: DecisionTreeClassifier(max_depth, min_samples_leaf), 5, 0.5
-    )
-    bagging.fit(X, y)
-    pred_proba = bagging.predict_proba(X)
-    pred = bagging.predict(X)
-    score = f1_score(y, pred, average="macro")
-    acc = accuracy_score(y, pred)
-    auc = roc_auc_score(y, pred_proba, multi_class="ovr")
-    print(f"bagging -> F1: {score:.2f} accuracy: {acc:.2%} AUC {auc:.2f}")
-
-    random_forest = RandomForestClassifier(5, 0.5, max_depth)
-    random_forest.fit(X, y)
-    pred_proba = random_forest.predict_proba(X)
-    pred = random_forest.predict(X)
-    score = f1_score(y, pred, average="macro")
-    acc = accuracy_score(y, pred)
-    auc = roc_auc_score(y, pred_proba, multi_class="ovr")
-    print(f"random forest -> F1: {score:.2f} accuracy: {acc:.2%} AUC {auc:.2f}")
-
-    adaboost = AdaboostClassifier(lambda: DecisionTreeClassifier(1, 0), 10, 0.5)
-    adaboost.fit(X, y)
-    pred_proba = adaboost.predict_proba(X)
-    pred = adaboost.predict(X)
-    score = f1_score(y, pred, average="macro")
-    acc = accuracy_score(y, pred)
-    auc = roc_auc_score(y, pred_proba, multi_class="ovr")
-    print(f"AdaBoost -> F1: {score:.2f} accuracy: {acc:.2%} AUC {auc:.2f}")
-
-    boost = GradientBoostingClassifier(lambda: DecisionTreeRegressor(1, 0), 10, 0.3)
-    boost.fit(X, y)
-    pred_proba = boost.predict_proba(X)
-    pred = boost.predict(X)
-    score = f1_score(y, pred, average="macro")
-    acc = accuracy_score(y, pred)
-    auc = roc_auc_score(y, pred_proba, multi_class="ovr")
-    print(f"Gradient boosting -> F1: {score:.2f} accuracy: {acc:.2%} AUC {auc:.2f}")
+    print_tree(tree._root_node)
+    print(f"regression tree -> MSE: {mse:.2f}")
 
 
 if __name__ == "__main__":
